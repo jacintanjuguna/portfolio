802cda06f569c5fef198128fa993307400657fe5
Jacinta Njuguna <102421883+jacintanjuguna@users.noreply.github.com>
2022-09-13 15:15:18 +0300

    Add files via upload

---
+++ src/Components/WorkCardData.js
@@ -0,0 +1,27 @@
+import projCard1 from '../assets/project-card1.png';
+import projCard2 from '../assets/project-card2.png';
+import projCard3 from '../assets/project-card3.png';
+
+const ProjectCardData = [{
+        imgsrc: projCard1,
+        title: 'E-Commerce Website',
+        text: 'A responsive e-commerce website built using ReactJS, and CSS. The website is hosted on Vercel. The website is fully functional and has all the features of an e-commerce website. The website is fully responsive and can be used on any device. The website has a user authentication system and a payment gateway.'
+            /*The website also has a chatbot which can be used to answer any queries. The website also has a dashboard for the admin to manage the products and orders. The website also has a dashboard for the user to manage their orders and profile.*/
+            ,
+        view: '#https://beautifly-app.vercel.app/',
+    },
+    {
+        imgsrc: projCard2,
+        title: 'Portfolio Website',
+        text: 'A responsive portfolio website built using ReactJs and CSS.The website is hosted on Vercel.The website is fully responsive and can be used on any device.The website uses an API to receive message sent on the contact section via sms. The website also has a dashboard for the admin to manage the messages. ',
+        view: '#'
+    },
+    {
+        imgsrc: projCard3,
+        title: 'Movie Search App',
+        text: 'A responsive movie search app built using JavaScript, Html, and CSS.The website is hosted on Github.The website is fully responsive and can be used on any device.The website uses an API to fetch the movies and display them on the website.  ',
+        view: '#'
+    }
+];
+
+export default ProjectCardData;
